f7d0c2134548f3e8a653e0758fa45c8a0dc0494c
Matthias Gerhard <matthias.gerhard00@gmail.com>
2023-06-14 21:12:31 +0200

    user fix

--- src/controllers/browse-controller.js
+++ src/controllers/browse-controller.js
@@ -3,15 +3,13 @@ import {weatherService} from "../services/weather-service.js";
 export const browseController = {
     index: {
         handler: async function (request, h) {
-            let users = await db.userStore.getAllUsers();
             let myCategories = await db.categoryStore.getUserCategories(request.auth.credentials._id);
             let placemarks = await db.placemarkStore.getAllPlacemarks();
             placemarks = placemarks.filter(placemark => placemark.category.user._id.toString() !== request.auth.credentials._id.toString());
             placemarks = await weatherService.getWeatherInfos(placemarks);
-            placemarks.forEach(placemark => {
-                let user = users.filter(user => user._id.toString() === placemark.category.user.toString());
-                placemark.category.user = user[0];
-            });
+            for (let placemark of placemarks) {
+                placemark.category.user = await db.userStore.getUserById(placemark.category.user);
+            }
             return h.view("Browse", {title: "Placemark - Browse", placemarks: placemarks, myCategories: myCategories});
         },
     },
